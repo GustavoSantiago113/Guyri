c36506dd394d11569b31f1431096ae40f7e38aa4
GustavoSantiago113 <gustavosantiago@usp.br>
2022-11-22 18:41:33 -0600

    Shiny dashboard não é o melhor

--- Versão 3/Guyri/server.R
+++ Versão 3/Guyri/server.R
@@ -1,7 +1,28 @@
 # Importando Bibliotecas ####
 library(shiny)
 library(shinyjs)
+
+# Analise Economica ####
+analiseEconomica <- function (Vi, Ve, Td, Go, Rla){
   
+  paySimples <- Vi/Vo
+  
+  
+  vectorPaySimples <- c()
+  controlPaySimples <- Vi;
+  vectorPaySimples <- append(vectorPaySimples, controlPaySimples)
+  for(i in round(paySimples)){
+    controlPaySimples = controlPaySimples - Vo;
+    vectorPaySimples <- append(vectorPaySimples, controlPaySimples);
+  }
+  
+  Von = Vo / (1 + (Td/100));
+  payDesco = Vi / Von;
+  
+  
+  
+}
+
   # Server ####
 shinyServer(function(input, output, session) {
   
